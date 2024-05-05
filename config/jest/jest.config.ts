14c14bf5cad8f96ee480239d5d43b7799f01a438
Eilla <eilla@student.21-school.ru>
2024-05-05 20:40:26 +0700

    tests

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -1,3 +1,5 @@
+import path from "path";
+
 export default {
   // All imported modules in your tests should be mocked automatically
   // automock: false,
@@ -14,9 +16,15 @@ export default {
   coveragePathIgnorePatterns: ["\\\\node_modules\\\\"],
   moduleFileExtensions: ["js", "jsx", "ts", "tsx", "json", "node"],
   moduleDirectories: ["node_modules"],
+  modulePaths: ["<rootDir>src"],
   testMatch: [
     // Обнаружил разницу между МАК ОС и ВИНДОУС!!!
     "<rootDir>src/**/*(*.)@(spec|test).[tj]s?(x)",
   ],
   rootDir: "../../",
+  setupFilesAfterEnv: ["<rootDir>config/jest/setupTests.ts"],
+  moduleNameMapper: {
+    "\\.(s?css)$": "identity-obj-proxy",
+    "\\.svg": path.resolve(__dirname, "jestEmptyComponent.tsx"),
+  },
 };
--- src/widgets/Sidebar/ui/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar.tsx
@@ -2,8 +2,10 @@ import { classNames } from "shared/lib/classNames/classNames";
 import { useState } from "react";
 import { ThemeSwitcher } from "widgets/ThemeSwitcher";
 import { LangSwitcher } from "widgets/LangSwitcher";
+
 import { useTranslation } from "react-i18next";
 import cls from "./Sidebar.module.scss";
+import Button from "../../../shared/ui/Button/Button";
 
 interface SidebarProps {
   className?: string;
@@ -17,13 +19,14 @@ const Sidebar = ({ className }: SidebarProps) => {
   };
   return (
     <div
+      data-testid="sidebar"
       className={classNames(cls.Sidebar, { [cls.collapsed]: collapsed }, [
         className,
       ])}
     >
-      <button type="button" onClick={onToggle}>
+      <Button data-testid="sidebar-toggle" type="button" onClick={onToggle}>
         {t("toggle")}
-      </button>
+      </Button>
       <div className={cls.switchers}>
         <ThemeSwitcher />
         <LangSwitcher className={cls.lang} />
